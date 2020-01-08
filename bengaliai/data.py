1c3527d5cc3ceef594fc6a7c4218efca819038d1
bhavika <bhavicka.7992@gmail.com>
2020-01-08 10:13:30 -0500

    Better dataset

--- bengaliai/data.py
+++ bengaliai/data.py
@@ -1,6 +1,7 @@
 from fastai import train
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
+from joblib import Parallel, delayed
 from bengaliai.config import (
     HEIGHT,
     WIDTH,
@@ -16,14 +17,12 @@ import numpy as np
 import cv2
 import os
 import tqdm
+from skimage.io import imsave
 
 
 class BengaliAIDataset(Dataset):
     def __init__(self, fname):
         self.df = pd.read_parquet(fname)
-        self.data = 255 - self.df.iloc[:, 1:].values.reshape(-1, HEIGHT, WIDTH).astype(
-            np.uint8
-        )
 
     def bbox(self, img):
         rows = np.any(img, axis=1)
@@ -32,16 +31,16 @@ class BengaliAIDataset(Dataset):
         cmin, cmax = np.where(cols)[0][[0, -1]]
         return rmin, rmax, cmin, cmax
 
-    def crop_resize(self, img, size=SIZE, pad=16):
+    def crop_resize(self, img0, size=SIZE, pad=16):
         # crop a box around pixels large than the threshold
         # some images contain line at the sides
-        ymin, ymax, xmin, xmax = self.bbox(img[5:-5, 5:-5] > 80)
+        ymin, ymax, xmin, xmax = self.bbox(img0[5:-5, 5:-5] > 80)
         # cropping may cut too much, so we need to add it back
         xmin = xmin - 13 if (xmin > 13) else 0
         ymin = ymin - 10 if (ymin > 10) else 0
         xmax = xmax + 13 if (xmax < WIDTH - 13) else WIDTH
         ymax = ymax + 10 if (ymax < HEIGHT - 10) else HEIGHT
-        img = img[ymin:ymax, xmin:xmax]
+        img = img0[ymin:ymax, xmin:xmax]
         # remove lo intensity pixels as noise
         img[img < 28] = 0
         lx, ly = xmax - xmin, ymax - ymin
@@ -55,23 +54,31 @@ class BengaliAIDataset(Dataset):
 
     def __getitem__(self, idx):
         name = self.df.iloc[idx, 0]
-        img = (self.data[idx] * (255.0 / self.data[idx].max())).astype(np.uint8)
+        data = 255 - self.df.iloc[:, 1:].values.reshape(-1, HEIGHT, WIDTH).astype(np.uint8)
+        img = (data[idx] * (255.0 / data[idx].max())).astype(np.uint8)
         img = self.crop_resize(img)
-        img = (img.astype(np.float32) / 255.0 - stats[0]) / stats[1]
         return img, name
 
 
 def create_train_images():
-    for f in TRAIN:
+    def write_img(item, ds_id):
+        img, name = item[0], item[1]
+        try:
+            cv2.imwrite(os.path.join(DATA_DIR, "train", f"{name}_{ds_id}.png"), img)
+        except Exception as e:
+            print(f"Could not save image {name} due to {e}")
+
+    def dataset_to_img(f, ds_id):
         ds = BengaliAIDataset(f)
-        for idx, item in tqdm.tqdm(enumerate(ds, 0)):
-            img, name = item[0], item[1]
-            img = cv2.imencode(".png", img)[1]
-            if not cv2.imwrite(os.path.join(DATA_DIR, "train", f"{name}.png"), img):
-                raise Exception(f"Could not write image {name}")
+        Parallel(n_jobs=6)(delayed(write_img)(item, ds_id) for _, item in tqdm.tqdm(enumerate(ds, 0)))
+
+    Parallel(n_jobs=3)(delayed(dataset_to_img)(f, i) for i, f in enumerate(TRAIN))
 
 
 def get_train():
     df = pd.read_csv(train_df_pth)
     nunique = list(df.nunique())[1:-1]
     return df, nunique
+
+
+create_train_images()
